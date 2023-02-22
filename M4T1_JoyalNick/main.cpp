067adf236a4cb6524d63ba9126bf38cb99ec8f6d
TheJoyal <81927754+TheJoyal@users.noreply.github.com>
2023-02-22 11:24:21 -0500

    M4T1 Part 2

--- M4T1_JoyalNick/main.cpp
+++ M4T1_JoyalNick/main.cpp
@@ -19,6 +19,13 @@ void say_hello(int count)
   return; // for void functions, return is optional (implied)
 }
 
+int square(int num)
+{
+  // input: num, and int
+  // output: num squared
+  return num*num;
+}
+
 
 int main() 
 {
@@ -27,4 +34,23 @@ int main()
   cout << "How many times should I say Hello? ";
   cin >> times;
   say_hello(times);
+
+
+
+
+// part 2 -- table of squares
+// The world's first "real program" running on a stored-program
+// computer (David Wheeler, Cambridge, May 6, 1949)
+
+int i = 0;
+int max;
+cout << "How many numbers in the table of squares? ";
+cin >> max;
+cout << "Table of squares from 0 to " << max << endl;
+cout << "____________________" << endl;
+while (i <= max)
+  {
+    cout << i << "\t\t" << square(i) << endl;
+    i++;
+  }
 }
