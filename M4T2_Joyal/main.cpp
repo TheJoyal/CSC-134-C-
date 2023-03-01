70701d5d39e58d2bc3f7d3dab8ffc9d1dd5c7f94
TheJoyal <nickjoyal@outlook.com>
2023-03-01 11:35:17 -0500

    Added 3d_6
    Added 3d_6 for character generation

--- M4T2_Joyal/main.cpp
+++ M4T2_Joyal/main.cpp
@@ -2,9 +2,13 @@
 // M4T2 - Menus
 // Joyal, Nick
 // 02/27/2023
+// version 2 -- character creator
+// Thudd the Barbarian
 
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -30,14 +34,27 @@ void option_one()
 }
 void option_two()
 {
-    // sum up five numbers
-    cout << "Test 2" << endl;
+    // sum up roll dice
+    int d6_1, d6_2, d6_3; // 3 dice (6 sided)
+    int total; // 3d6
+    int seed;
+    seed = time(0);
+    srand(seed);
+    d6_1 = (rand() % 6) + 1;
+    d6_2 = (rand() % 6) + 1;
+    d6_3 = (rand() % 6) + 1;
+    total = d6_1 + d6_2 + d6_3;
+    cout << "Rolling 3d6: ";
+    cout << d6_1 << " + " << d6_2 << " + " << d6_3;
+    cout << " = " << total << endl;
 }
 void option_three()
 {
     // TODO what does this do
     cout << "Test 3" << endl;
 }
+
+// option four = quit
 
 int main()
 {
@@ -47,24 +64,37 @@ int main()
 
     cout << "Welcome to the menu" << endl;
     cout << "Press 1 to count" << endl;
-    cout << "Press 2 to sum up numbers" << endl;
-    cout << "Press 3 to TODO" << endl; // not implemented yet
+    cout << "Press 2 to roll dice" << endl;
+    cout << "Press 3 to roll a character" << endl; // not implemented yet
+    cout << "Press 4 to quit" << endl;
     cout << "Choice: ";
 
 
     // TODO input validation
     int choice;
     cin >> choice;
-    // user must pick 1, 2, or 3
-    while(choice < 1 || choice > 3)
+    // user must pick 1, 2, 3, or 4
+    while(choice < 1 || choice > 4)
     {
-        cout << "Please choose 1, 2, or 3: ";
+        cout << "Please choose 1, 2, 3, or 4: ";
         cin >> choice;
     }
     // if we get here, we know the choice is valid
     if(choice == 1)
     {
         option_one(); // function call
+    }
+    if(choice == 2)
+    {
+        option_two();
+    }
+    if(choice == 3)
+    {
+        option_three();
+    }
+    if(choice == 4)
+    {
+        cout << "Quitting time!" << endl;
     }
 
 
